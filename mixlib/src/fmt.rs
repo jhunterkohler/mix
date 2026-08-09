9a43e33694ee5c56e90f4c23b13ae7ce2ef62d3c
John Hunter Kohler <jhunterkohler@gmail.com>
2026-08-08 22:54:26 -0400

    Rework mixlib formatting

--- mixlib/src/fmt.rs
+++ mixlib/src/fmt.rs
@@ -1,133 +1,187 @@
-//! MIX code formatting.
-use std::iter::repeat_n;
+use std::str::Chars;
 
-/// Finds offset of first whitespace character at an index greater than or
-/// equal to `start`, or returns `s.len()` if no such character exists.
-///
-/// # Safety
-///
-/// * `start` must be on a valid byte boundary.
-/// * `start` must be <= s.len()
-unsafe fn find_ws(s: &str, start: usize) -> usize {
-    // SAFETY: `start..` is valid index by function preconditions.
-    unsafe { s.get_unchecked(start..) }
-        .find(char::is_whitespace)
-        .map(|index| index + start)
-        .unwrap_or(s.len())
+use crate::source::Span;
+
+const SPACES: [&str; 12] = [
+    "",
+    " ",
+    "  ",
+    "   ",
+    "    ",
+    "     ",
+    "      ",
+    "       ",
+    "        ",
+    "         ",
+    "          ",
+    "           ",
+];
+
+struct Formatter<'a> {
+    src: &'a str,
+    it: Chars<'a>,
+    empties: usize,
+    dest: &'a mut String,
 }
 
-/// Finds offset of first whitespace character at an index greater than or
-/// equal to `start`, or returns `s.len()` if no such character exists.
-///
-/// # Safety
-///
-/// * `start` must be on a valid byte boundary.
-/// * `start` must be <= s.len()
-unsafe fn find_non_ws(s: &str, start: usize) -> usize {
-    // SAFETY: `start..` is valid index by function preconditions.
-    unsafe { s.get_unchecked(start..) }
-        .find(|c: char| !c.is_whitespace())
-        .map(|index| index + start)
-        .unwrap_or(s.len())
-}
-
-/// Get parts of line to format: the loc, op, and address/comment portions.
-fn line_parts(line: &str) -> (&str, &str, &str) {
-    // SAFETY: Each call to `find_ws` and `find_non_ws` upholds character
-    // boundary preconditions for valid string indexes and slices into `line`.
-    unsafe {
-        let loc_end = find_ws(line, 0);
-        let op_start = find_non_ws(line, loc_end);
-        let op_end = find_ws(line, op_start);
-        let after_op_start = find_non_ws(line, op_end);
-
-        (
-            line.get_unchecked(..loc_end),
-            line.get_unchecked(op_start..op_end),
-            line.get_unchecked(after_op_start..),
-        )
-    }
-}
-
-/// Format line containing code that is nonempty and end-trimmed.
-fn format_line(line: &str, dest: &mut String) {
-    debug_assert!(line.trim_end() == line);
-    debug_assert!(!line.is_empty());
-
-    let (loc, op, after_op) = line_parts(line);
-
-    dest.push_str(loc);
-
-    // If `op` is empty, then `after_op` must also be, and we are done.
-    if op.is_empty() {
-        return;
+impl<'a> Formatter<'a> {
+    fn new(src: &'a str, dest: &'a mut String) -> Self {
+        let src = src.trim();
+        Self { src, it: src.chars(), empties: 0, dest }
     }
 
-    // Pad `loc` to 10 characters with spaces and add one separation space.
-    if loc.len() < 10 {
-        dest.extend(repeat_n(' ', 11 - loc.len()));
-    } else {
-        dest.push(' ');
+    /// Gets current position.
+    fn pos(&self) -> usize {
+        self.src.len() - self.it.as_str().len()
     }
 
-    dest.push_str(op);
-
-    // We are done if nothing after `op`.
-    if after_op.is_empty() {
-        return;
+    /// Get the next character of input without consuming it.
+    fn peek(&self) -> Option<char> {
+        self.it.clone().next()
     }
 
-    // Pad `op` to 4 characters with spaces and add one separation space.
-    if op.len() < 4 {
-        dest.extend(repeat_n(' ', 5 - op.len()));
-    } else {
-        dest.push(' ');
+    /// Get the 2nd next character of input without consuming anything.
+    fn peek2(&self) -> Option<char> {
+        self.it.clone().nth(1)
     }
 
-    dest.push_str(after_op);
-}
+    /// Get the next character of input and consume it.
+    fn next(&mut self) -> Option<char> {
+        self.it.next()
+    }
 
-/// Format MIX assembly.
-///
-/// The formatted `src` is appended to `dest`.
-///
-/// # Examples
-///
-/// Basic usage:
-///
-/// ```
-/// use mixlib::fmt::format_code;
-///
-/// let mut dest = "* COMMENT LINE\n".to_string();
-/// format_code("L EQU 500    comment\n\n", &mut dest);
-///
-/// assert_eq!(&dest, "* COMMENT LINE\nL          EQU  500    comment\n");
-/// ```
-pub fn format_code(src: &str, dest: &mut String) {
-    let src = src.trim();
-    let mut last_empty = false;
-
-    for line in src.lines().map(str::trim_end) {
-        if line.is_empty() {
-            if !last_empty {
-                dest.push('\n');
-                last_empty = true;
+    /// Ignore the next sequence of inline whitespace characters.
+    fn ignore_inline_whitespace(&mut self) {
+        loop {
+            match self.peek() {
+                // "\r\n" style newline
+                Some('\r') if self.peek2() == Some('\n') => break,
+                // "\n" style newline
+                Some('\n') => break,
+                Some(c) if c.is_whitespace() => {
+                    self.next();
+                }
+                _ => break,
             }
-        } else if line.starts_with('*') {
-            dest.push_str(line);
-            dest.push('\n');
-            last_empty = false;
-        } else {
-            format_line(line, dest);
-            dest.push('\n');
-            last_empty = false;
         }
     }
+
+    /// Get the span of the next series of non-whitespace characters.
+    fn get_non_whitespace(&mut self) -> Span {
+        let start = self.pos();
+        while self.peek().is_some_and(|c| !c.is_whitespace()) {
+            self.next();
+        }
+        Span::new(start, self.pos())
+    }
+
+    /// Get a comment starting at the current position. Ignores other
+    /// whitespace to the start of next line.
+    fn get_comment_to_nextline(&mut self) -> Span {
+        let start = self.pos();
+        let mut end = start;
+
+        loop {
+            match self.peek() {
+                None => break,
+                Some('\n') => {
+                    self.next();
+                    break;
+                }
+                Some('\r') if self.peek2() == Some('\n') => {
+                    self.next();
+                    self.next();
+                    break;
+                }
+                Some(c) if !c.is_whitespace() => {
+                    self.next();
+                    end = self.pos();
+                }
+                _ => {
+                    self.next();
+                }
+            }
+        }
+
+        Span::new(start, end)
+    }
+
+    fn format(mut self) {
+        if self.src.is_empty() {
+            self.dest.push('\n');
+        } else {
+            loop {
+                match self.peek() {
+                    Some('*') => self.format_comment_line(),
+                    Some(_) => self.format_code_line(),
+                    None => break,
+                }
+            }
+        }
+    }
+
+    fn format_comment_line(&mut self) {
+        let span = self.get_comment_to_nextline();
+        self.dest.push_str(&self.src[span]);
+        self.dest.push('\n');
+        self.empties = 0;
+    }
+
+    fn format_code_line(&mut self) {
+        let loc = self.get_non_whitespace();
+        self.ignore_inline_whitespace();
+        let op = self.get_non_whitespace();
+        self.ignore_inline_whitespace();
+        let addr = self.get_non_whitespace();
+        let comment = self.get_comment_to_nextline();
+
+        if op.is_empty() {
+            if loc.is_empty() {
+                // Entire line is empty.
+                return self.format_empty_line();
+            } else {
+                // There is only a LOC.
+                self.dest.push_str(&self.src[loc]);
+            }
+        } else {
+            // LOC is given 10 columns.
+            let spaces = SPACES[11usize.saturating_sub(loc.len()).max(1)];
+            self.dest.push_str(&self.src[loc]);
+            self.dest.push_str(spaces);
+            self.dest.push_str(&self.src[op]);
+
+            if !addr.is_empty() {
+                // OP is given 4 columns.
+                let spaces = SPACES[5usize.saturating_sub(op.len()).max(1)];
+                self.dest.push_str(spaces);
+                self.dest.push_str(&self.src[addr]);
+
+                if !comment.is_empty() {
+                    self.dest.push_str(&self.src[comment]);
+                }
+            }
+        }
+
+        self.dest.push('\n');
+        self.empties = 0;
+    }
+
+    fn format_empty_line(&mut self) {
+        // Allow at most 2 empty lines in sequence.
+        if self.empties < 2 {
+            self.dest.push('\n');
+        }
+        self.empties += 1;
+    }
 }
 
-pub fn format_code_to_string(src: &str) -> String {
+pub fn format(src: &str, dest: &mut String) {
+    Formatter::new(src, dest).format()
+}
+
+pub fn format_to_string(src: &str) -> String {
     let mut dest = String::new();
-    format_code(src, &mut dest);
+    format(src, &mut dest);
     dest
 }
 
@@ -135,36 +189,32 @@ pub fn format_code_to_string(src: &str) -> String {
 mod tests {
     use super::*;
 
-    const UNFORMATTED: &str = concat!(
-        "\n\n",
-        "* COMMENT LINE  \n",
-        "A  \n",
-        "A EQU  \n",
-        "ABCDEFGHJK    EQU  \n",
-        "ABCDEFGHJKL    EQU  \n",
-        "ABCDEFGHJKL    EQU    AFTER  \n",
-        "ABCDEFGHJKL    ORIG    AFTER \n",
-        "ABCDEFGHJKL    ABCDE    AFTER  \n",
-        "\n\n",
-        "* COMMENT LINE\n",
-        "\n\n",
-    );
-
-    const FORMATTED: &str = concat!(
-        "* COMMENT LINE\n",
-        "A\n",
-        "A          EQU\n",
-        "ABCDEFGHJK EQU\n",
-        "ABCDEFGHJKL EQU\n",
-        "ABCDEFGHJKL EQU  AFTER\n",
-        "ABCDEFGHJKL ORIG AFTER\n",
-        "ABCDEFGHJKL ABCDE AFTER\n",
-        "\n",
-        "* COMMENT LINE\n",
-    );
+    const TEST_CASES: [(&str, &str); 14] = [
+        ("", "\n"),
+        ("\n", "\n"),
+        ("\n\n", "\n"),
+        ("\x0b \n\t \r\n ", "\n"),
+        ("* COMMENT  \n  ", "* COMMENT\n"),
+        ("* COMMENT", "* COMMENT\n"),
+        ("A    \n", "A\n"),
+        ("A EQU  \n", "A          EQU\n"),
+        ("ABCDEFGHIJ    EQU \n", "ABCDEFGHIJ EQU\n"),
+        ("ABCDEFGHIJK    EQU \n", "ABCDEFGHIJK EQU\n"),
+        ("ABC EQU ADDR  \n", "ABC        EQU  ADDR\n"),
+        ("ABC ORIG2 ADDR  \n", "ABC        ORIG2 ADDR\n"),
+        ("ABC EQU ADDR COMMENT \n", "ABC        EQU  ADDR COMMENT\n"),
+        ("ABC EQU ADDR   COMMENT \n", "ABC        EQU  ADDR   COMMENT\n"),
+    ];
 
     #[test]
     fn format_code_follows_style() {
-        assert_eq!(format_code_to_string(UNFORMATTED), FORMATTED);
+        for (src, expected) in TEST_CASES {
+            let dest1 = format_to_string(src);
+            let mut dest2 = String::new();
+            format(src, &mut dest2);
+
+            assert_eq!(dest1, expected);
+            assert_eq!(dest2, expected);
+        }
     }
 }
