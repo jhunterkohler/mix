1d71c08271e0b6b16bd436e4f23a2ab78bc9bdb8
John Hunter Kohler <jhunterkohler@gmail.com>
2026-08-20 13:02:21 -0400

    Reword instruction and op

--- mixlib/src/asm/mod.rs
+++ mixlib/src/asm/mod.rs
@@ -1,14 +1,16 @@
 //! MIX assembly.
 mod assembler;
+// mod instruction;
 mod instruction;
-mod op;
+// mod op;
 mod opcode;
 mod program;
 mod pseudo_op;
 
 pub use assembler::*;
+// pub use instruction::*;
+// pub use op::*;
 pub use instruction::*;
-pub use op::*;
 pub use opcode::*;
 pub use program::*;
 pub use pseudo_op::*;
